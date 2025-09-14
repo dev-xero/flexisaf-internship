e5eac3ed3e1e6459788f8ae10a2359ee0b5623ba
= <ssh.xero@gmail.com>
2025-09-15 00:44:41 +0100

    feat: complete task-1
    Signed-off-by: = <ssh.xero@gmail.com>

---
+++ Task-1/p/springboot-setup/src/test/java/io/github/dev/xero/springbootsetup/SpringbootSetupApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dev.xero.springbootsetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSetupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
