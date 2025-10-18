517366471c699b5a1bcabe7482141873fa1923a9
= <ssh.xero@gmail.com>
2025-10-18 07:12:22 +0100

    task 5 (init): setup springboot project

---
+++ Task-6/DI/src/main/java/github/dev/xero/di/DiApplication.java
@@ -0,0 +1,13 @@
+package github.dev.xero.di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiApplication.class, args);
+    }
+
+}
