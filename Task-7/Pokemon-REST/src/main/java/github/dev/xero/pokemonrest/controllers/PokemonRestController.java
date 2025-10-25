6e8273f6f8521f3b9d4f7a56d5f414222c106e55
= <ssh.xero@gmail.com>
2025-10-25 17:53:12 +0100

    feat (task 7): Separate logic into service layer from controller

--- Task-7/Pokemon-REST/src/main/java/github/dev/xero/pokemonrest/controllers/PokemonRestController.java
+++ Task-7/Pokemon-REST/src/main/java/github/dev/xero/pokemonrest/controllers/PokemonRestController.java
@@ -6,7 +6,10 @@ import github.dev.xero.pokemonrest.models.PokemonModel;
 import github.dev.xero.pokemonrest.services.PokemonService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
+import org.apache.coyote.BadRequestException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,19 +28,37 @@ public class PokemonRestController {
     }
 
     @GetMapping("/")
-    @Operation(
-            summary = "Default base endpoint."
-    )
+    @Operation(summary = "Default base endpoint.")
     public ResponseEntity<HttpResponse<String>> get() {
-        return pokemonService.base();
+        return ResponseEntity.ok(
+                HttpResponse.Ok("Pokemon API v1", "/", null)
+        );
     }
 
     @PostMapping("/create")
-    @Operation(
-            summary = "Create a new Pokemon"
-    )
-    public ResponseEntity<HttpResponse<PokemonModel>> post(@Valid @RequestBody CreatePokemonDto dto) {
-        return pokemonService.create(dto);
+    @Operation(summary = "Create a new Pokemon")
+    public ResponseEntity<HttpResponse<PokemonModel>> post(
+            @Valid @RequestBody CreatePokemonDto dto,
+            HttpServletRequest req
+    ) {
+        try {
+            PokemonModel pokemon = pokemonService.create(dto);
+            return ResponseEntity.ok(
+                    HttpResponse.Ok(
+                            "Pokemon created!",
+                            req.getRequestURI(),
+                            pokemon
+                    )
+            );
+        } catch (BadRequestException ex) {
+           return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                   HttpResponse.BadRequest(
+                           ex.getMessage(),
+                           "Cannot create duplicate Pokemon",
+                           req.getRequestURI()
+                   )
+           );
+        }
     }
 
 }
--- Task-7/Pokemon-REST/src/main/java/github/dev/xero/pokemonrest/services/PokemonService.java
+++ Task-7/Pokemon-REST/src/main/java/github/dev/xero/pokemonrest/services/PokemonService.java
@@ -4,6 +4,7 @@ import github.dev.xero.pokemonrest.dto.HttpResponse;
 import github.dev.xero.pokemonrest.dto.pokemon.CreatePokemonDto;
 import github.dev.xero.pokemonrest.models.PokemonModel;
 import github.dev.xero.pokemonrest.repositories.PokemonRepository;
+import org.apache.coyote.BadRequestException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -16,34 +17,15 @@ public class PokemonService {
         this.pokemonRepository = pokemonRepository;
     }
 
-    public ResponseEntity<HttpResponse<String>> base() {
-        return ResponseEntity.ok(
-                HttpResponse.Ok("Pokemon API v1", "/", null)
-        );
-    }
-
-    public ResponseEntity<HttpResponse<PokemonModel>> create(CreatePokemonDto dto) {
+    public PokemonModel create(CreatePokemonDto dto) throws BadRequestException {
         // Validation: make sure a pokemon with the same name doesn't already exist.
         if (pokemonRepository.findByName(dto.getName()) != null) {
-            return ResponseEntity
-                    .badRequest()
-                    .body(HttpResponse.BadRequest(
-                            "A Pokemon with this name already exists!",
-                            "Cannot create duplicate Pokemon",
-                            "/create"
-                    )
-            );
+            throw new BadRequestException("A pokemon with this name already exists!");
         }
         PokemonModel pokemon = new PokemonModel();
         pokemon.setName(dto.getName());
         pokemon.setType(dto.getType());
         pokemonRepository.save(pokemon);
-        return ResponseEntity.ok(
-                HttpResponse.Ok(
-                        "Pokemon created!",
-                        "/create",
-                        pokemon
-                )
-        );
+        return pokemon;
     }
 }
